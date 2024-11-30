7ac89a3c668004ae108f521b965e0782383eb3bc
Doxy <doxy@doxy.fyi>
2024-11-29 18:24:50 -0800

    Surface Creation
    Also allows a surface to be configured!

---
+++ webgpu/device.cpp
@@ -0,0 +1,15 @@
+#include "device.hpp"
+#include "surface.hpp"
+
+namespace stylizer::api::webgpu {
+	device device::create_default(create_config config) {
+		device out;
+		out.adapter = get_instance().requestAdapter(WGPURequestAdapterOptions{
+			.compatibleSurface = config.compatible_surface ? confirm_wgpu_type<webgpu::surface>(*config.compatible_surface).surface_ : nullptr,
+			.powerPreference = config.high_performance ? wgpu::PowerPreference::HighPerformance : wgpu::PowerPreference::LowPower,
+		});
+		out.device_ = out.adapter.requestDevice({});
+		out.queue = out.device_.getQueue();
+		return out;
+	}
+}
