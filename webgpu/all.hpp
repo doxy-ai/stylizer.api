1387e5031a4d68a00dcff98d11409b50b2f8af3f
Doxy <doxy@doxy.fyi>
2024-12-08 21:40:41 -0800

    Initial Compute Support (And Surrounding Structure)
    Reworks the command encoder in a compute "render pass" and implements compute pipelines.
    Also adds additional functionality to map and copy buffers.

--- webgpu/all.hpp
+++ webgpu/all.hpp
@@ -2,7 +2,9 @@
 
 #include "device.hpp"
 #include "surface.hpp"
-#include "render_pass.hpp"
 #include "buffer.hpp"
 #include "shader.hpp"
-#include "graphics_pipeline.hpp"
+#include "render_pass.hpp"
+#include "command_encoder.hpp"
+#include "render_pipeline.hpp"
+#include "compute_pipeline.hpp"
