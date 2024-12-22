07d3b3263adc2b5b67b8a8430bb51951e6726949
Doxy <doxy@doxy.fyi>
2024-12-22 14:24:16 -0800

    Initial Render Pass Support
    Supports creating render passes with color and depth/stencil attachments.
    Also supports ending a render pass to get a compute buffer, and submitting that compute buffer.

--- webgpu/device.hpp
+++ webgpu/device.hpp
@@ -3,6 +3,9 @@
 #include "common.hpp"
 
 namespace stylizer::api::webgpu {
+	struct texture;
+	struct render_pass;
+
 	struct device: public api::device { STYLIZER_API_GENERIC_AUTO_RELEASE_SUPPORT(device);
 		char type[4] = STYLIZER_API_WGPU_TYPE;
 		wgpu::Adapter adapter = nullptr;
@@ -33,6 +36,14 @@ namespace stylizer::api::webgpu {
 			// }
 		}
 
+		webgpu::texture create_texture(const api::texture::create_config& config = {});
+		api::texture& create_texture(temporary_return_t, const api::texture::create_config& config = {}) override;
+		webgpu::texture create_and_write_texture(std::span<const std::byte> data, const api::texture::data_layout& layout, const api::texture::create_config& config = {});
+		api::texture& create_and_write_texture(temporary_return_t, std::span<const std::byte> data, const api::texture::data_layout& layout, const api::texture::create_config& config = {}) override;
+
+		webgpu::render_pass create_render_pass(std::span<api::render_pass::color_attachment> colors, std::optional<api::render_pass::depth_stencil_attachment> depth = {}, bool one_shot = false, std::string_view label = "Stylizer Render Pass");
+		api::render_pass& create_render_pass(temporary_return_t, std::span<api::render_pass::color_attachment> colors, std::optional<api::render_pass::depth_stencil_attachment> depth = {}, bool one_shot = false, std::string_view label = "Stylizer Render Pass") override;
+
 		void release(bool static_sub_objects = false) override {
 			if(static_sub_objects) {
 				static auto_release<device> device = {};
@@ -44,5 +55,5 @@ namespace stylizer::api::webgpu {
 			if(adapter) std::exchange(adapter, nullptr).release();
 		}
 	};
-	static_assert(device_concept<device>);
+	// static_assert(device_concept<device>); // NOTE: In .cpp file
 }
---
+++ webgpu/render_pass.hpp
@@ -0,0 +1,96 @@
+#pragma once
+
+#include "command_buffer.hpp"
+#include "common.hpp"
+#include "texture.hpp"
+
+namespace stylizer::api::webgpu {
+	struct render_pass: public api::render_pass, public detail::command_encoder { STYLIZER_API_GENERIC_AUTO_RELEASE_SUPPORT(render_pass);
+		char type[4] = STYLIZER_API_WGPU_TYPE;
+		wgpu::CommandEncoder pass_encoder = nullptr;
+		wgpu::RenderPassEncoder pass = nullptr;
+
+		inline render_pass(render_pass&& o) { *this = std::move(o); }
+		inline render_pass& operator=(render_pass&& o) {
+			encoder = std::exchange(o.encoder, nullptr);
+			pass_encoder = std::exchange(o.pass_encoder, nullptr);
+			pass = std::exchange(o.pass, nullptr);
+			return *this;
+		}
+		inline operator bool() { return encoder || pass_encoder || pass; }
+
+		static render_pass create(api::device& device_, std::span<render_pass::color_attachment> colors, std::optional<render_pass::depth_stencil_attachment> depth = {}, bool one_shot = false, std::string_view label = "Stylizer Render Pass") {
+			assert(colors.size() > 0);
+			assert(!(depth.has_value() && depth->depth_clear_value.has_value()) || depth->depth_clear_value >= 0);
+			assert(!(depth.has_value() && depth->depth_clear_value.has_value()) || depth->depth_clear_value <= 0);
+			auto& device = confirm_wgpu_type<webgpu::device>(device_);
+
+			render_pass out;
+			out.encoder = device.device_.createCommandEncoder(WGPUCommandEncoderDescriptor{.label = (std::string(label) + " Pre Encoder").c_str()});
+			out.pass_encoder = device.device_.createCommandEncoder(WGPUCommandEncoderDescriptor{.label = (std::string(label) + " Encoder").c_str()});
+
+			std::vector<WGPURenderPassColorAttachment> color_attachments; color_attachments.reserve(colors.size());
+			for(auto& attach: colors) {
+				auto view = confirm_wgpu_type<webgpu::texture>(attach.texture).view;
+				color_attachments.emplace_back(WGPURenderPassColorAttachment{
+					.view = view,
+					.resolveTarget = attach.resolve_target ? confirm_wgpu_type<webgpu::texture>(*attach.resolve_target).view : nullptr,
+					.loadOp = attach.clear_value.has_value() ? wgpu::LoadOp::Clear : wgpu::LoadOp::Load,
+					.storeOp = attach.should_store ? wgpu::StoreOp::Store : wgpu::StoreOp::Discard,
+					.clearValue = attach.clear_value.has_value() ? to_wgpu(*attach.clear_value) : to_wgpu(color32{}),
+				});
+			}
+			WGPURenderPassDepthStencilAttachment* depth_attachment = nullptr;
+			if(depth) {
+				static WGPURenderPassDepthStencilAttachment static_depth;
+				static_depth = {
+					.view = confirm_wgpu_type<webgpu::texture>(depth->texture).view,
+					.depthLoadOp = depth->depth_clear_value.has_value() ? wgpu::LoadOp::Clear : wgpu::LoadOp::Load,
+					.depthStoreOp = depth->should_store_depth ? wgpu::StoreOp::Store : wgpu::StoreOp::Discard,
+					.depthClearValue = depth->depth_clear_value.has_value() ? *depth->depth_clear_value : 1,
+					.depthReadOnly = depth->depth_readonly,
+					.stencilLoadOp = depth->stencil_clear_value.has_value() ? wgpu::LoadOp::Clear : wgpu::LoadOp::Load,
+					.stencilStoreOp = depth->should_store_stencil ? wgpu::StoreOp::Store : wgpu::StoreOp::Discard,
+					.stencilClearValue = static_cast<uint32_t>(depth->stencil_clear_value.has_value() ? *depth->stencil_clear_value : 0),
+					.stencilReadOnly = depth->stencil_readonly,
+				};
+				depth_attachment = &static_depth;
+			}
+			out.pass = out.encoder.beginRenderPass(WGPURenderPassDescriptor{
+				.label = cstring_from_view(label),
+				.colorAttachmentCount = color_attachments.size(),
+				.colorAttachments = color_attachments.data(),
+				.depthStencilAttachment = depth_attachment,
+				.occlusionQuerySet = nullptr,
+				.timestampWrites = nullptr,
+			});
+
+			return out;
+		}
+
+		command_buffer end(api::device& device) {
+			pass.end();
+			command_buffer out;
+			out.pre = encoder.finish();
+			out.main = pass_encoder.finish();
+			return out;
+		}
+		command_buffer& end(temporary_return_t, api::device& device) override {
+			static command_buffer buffer;
+			return buffer = end(device);
+		}
+
+		void submit(api::device& device, bool release = true) override {
+			auto buffer = end(device);
+			buffer.submit(device, release);
+			if(release) this->release();
+		}
+
+		void release() override {
+			if(encoder) std::exchange(encoder, nullptr).release();
+			if(pass_encoder) std::exchange(pass_encoder, nullptr).release();
+			if(pass) std::exchange(pass, nullptr).release();
+		}
+	};
+	static_assert(render_pass_concept<render_pass>);
+}
