57b23aae57c0e1e8ddf8773a5d86a8f2eeb51e31
Doxy <doxy@doxy.fyi>
2025-11-29 11:23:58 -0800

    [WebGPU] Rendering
    Add support for both the render_pass and render_pipeline.

--- test.cpp
+++ test.cpp
@@ -34,15 +34,49 @@ int main() {
 	stylizer::auto_release<stylizer::api::current_backend::surface> surface;
 	std::tie(device, surface) = stylizer::api::sdl3::create_surface_and_device<stylizer::api::current_backend::device, stylizer::api::current_backend::surface>(window);
 
-	surface.configure(device, surface.determine_optimal_default_config(device, size));
+	surface.configure(device, surface.determine_optimal_default_config(device, {800, 600}));
 
-	std::vector<int> data = {1, 2, 3, 4};
-	auto b = device.create_and_write_buffer(stylizer::api::usage::MapRead, stylizer::byte_span<int>(data));
-	auto ints = (int*)b.map(device);
-	assert(ints[0] == 1);
-	assert(ints[1] == 2);
-	assert(ints[2] == 3);
-	assert(ints[3] == 4);
+	auto source = R"(
+[shader("vertex")]
+float4 vertex(uint index : SV_VertexID) : SV_Position {
+	float2 p = float2(0.0, 0.0);
+	if (index == 0) {
+		p = float2(-0.5, -0.5);
+	} else if (index == 1) {
+		p = float2(0.5, -0.5);
+	} else {
+		p = float2(0.0, 0.5);
+	}
+	return float4(p, 0.0, 1.0);
+}
+
+[shader("fragment")]
+float4 fragment(float4 : SV_Position) : SV_Target {
+    return float4(231.0/255, 39.0/255, 37.0/255, 1.0);
+})";
+
+	stylizer::auto_release vertex_shader = device.create_shader_from_source(
+		stylizer::api::shader::language::Slang,
+		stylizer::api::shader::stage::Vertex,
+		source, "vertex"
+	);
+	stylizer::auto_release fragment_shader = device.create_shader_from_source(
+		stylizer::api::shader::language::Slang,
+		stylizer::api::shader::stage::Fragment,
+		source, "fragment"
+	);
+
+	std::vector<stylizer::api::color_attachment> color_attachments = {{
+		.texture_format = surface.configured_texture_format(device),
+		.clear_value = {{2.f/255, 7.f/255, 53.f/255, 1.f}},
+	}};
+	stylizer::auto_release pipeline = device.create_render_pipeline(
+		{
+			{stylizer::api::shader::stage::Vertex, {&vertex_shader, "vertex"}},
+			{stylizer::api::shader::stage::Fragment, {&fragment_shader, "fragment"}}
+		},
+		color_attachments
+	);
 
 	bool should_close = false;
 	while (!should_close) {
@@ -50,9 +84,18 @@ int main() {
 		break; case SDL_EVENT_QUIT:
 			should_close = true;
 		}
+		device.process_events();
 
-		stylizer::auto_release texture = surface.next_texture(device);
-
-		surface.present(device);
+		try {
+			stylizer::auto_release texture = surface.next_texture(device);
+			color_attachments[0].texture = &texture;
+			device.create_render_pass(color_attachments, {}, true)
+				.bind_render_pipeline(device, pipeline)
+				.draw(device, 3)
+				.one_shot_submit(device);
+			surface.present(device);
+		} catch(stylizer::api::surface::texture_acquisition_failed fail) {
+			std::cerr << fail.what() << std::endl;
+		}
 	}
 }
