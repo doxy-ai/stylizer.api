1c4f4391a268e66de6466b0fcb8c08e0f6ad7745
Doxy <doxy@doxy.fyi>
2026-01-16 14:23:51 -0800

    Buffer

--- backends/webgpu/webgpu.hpp
+++ backends/webgpu/webgpu.hpp
@@ -78,7 +78,7 @@ namespace stylizer::api::webgpu {
 
 	struct buffer : public api::buffer { STYLIZER_API_GENERIC_AUTO_RELEASE_SUPPORT(buffer); STYLIZER_API_MOVE_TEMPORARY_TO_HEAP_DERIVED_METHOD(buffer);
 		uint32_t type = magic_number;
-		WGPUBuffer buffer_;
+		WGPUBuffer buffer_ = nullptr;
 
 		buffer(buffer&& o) { *this = std::move(o); }
 		buffer& operator=(buffer&& o) {
@@ -87,17 +87,17 @@ namespace stylizer::api::webgpu {
 		}
 		inline operator bool() const override { return buffer_; }
 
-		static buffer create(api::device& device, usage usage, size_t size, bool mapped_at_creation = false, const std::string_view label = "Stylizer Buffer");
+		static buffer create(api::device& device, enum usage usage, size_t size, bool mapped_at_creation = false, const std::string_view label = "Stylizer Buffer");
 
-		static buffer create_and_write(api::device& device, usage usage, std::span<const std::byte> data, size_t offset = 0, const std::string_view label = "Stylizer Buffer");
+		static buffer create_and_write(api::device& device, enum usage usage, std::span<const std::byte> data, size_t offset = 0, const std::string_view label = "Stylizer Buffer");
 		template<typename T>
 		requires(!std::same_as<T, std::byte>)
-		static buffer create_and_write(api::device& device, usage usage, std::span<const T> data, size_t offset = 0, const std::string_view label = "Stylizer Buffer") {
+		static buffer create_and_write(api::device& device, enum usage usage, std::span<const T> data, size_t offset = 0, const std::string_view label = "Stylizer Buffer") {
 			return create_and_write(device, usage, byte_span(data), offset, label);
 		}
 
-		static const buffer& zero_buffer(api::device& device, usage usage = usage::Storage, size_t minimum_size = 0, api::buffer* just_released = nullptr);
-		const api::buffer& get_zero_buffer_singleton(api::device& device, usage usage = usage::Storage, size_t size = 0, api::buffer* just_released = nullptr) override;
+		static const buffer& zero_buffer(api::device& device, enum usage usage = usage::Storage, size_t minimum_size = 0, api::buffer* just_released = nullptr);
+		const api::buffer& get_zero_buffer_singleton(api::device& device, enum usage usage = usage::Storage, size_t size = 0, api::buffer* just_released = nullptr) override;
 
 		api::buffer& write(api::device& device, std::span<const std::byte> data, size_t offset = 0) override;
 		template<typename T>
@@ -107,6 +107,7 @@ namespace stylizer::api::webgpu {
 		}
 
 		size_t size() const override;
+		enum usage usage() const override;
 
 		api::buffer& copy_from(api::device& device, const api::buffer& source, std::optional<size_t> destination_offset = 0, std::optional<size_t> source_offset = 0, std::optional<size_t> size_override = {}) override;
 
